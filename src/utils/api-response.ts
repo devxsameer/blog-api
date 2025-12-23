84ed33f631d9d9e3c7c01b59ac739bdd2bb34f35
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2025-12-23 15:46:59 +0530

    feat: add api-response util

---
+++ src/utils/api-response.ts
@@ -0,0 +1,13 @@
+import { Response } from "express";
+
+export function sendResponse(
+  res: Response,
+  {
+    statusCode = 200,
+    message = "Success",
+    data = null,
+    meta = null,
+  }: { statusCode?: number; message?: string; data?: any; meta?: any }
+) {
+  return res.status(statusCode).json({ success: "true", message, data, meta });
+}
